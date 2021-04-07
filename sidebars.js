73e2e50cc2d96ab34283aa58878c01204995fbb3
swarn <swarnchahal@Swarns-MacBook-Pro.local>
2021-04-06 22:03:19 -0400

    init git repo

---
+++ sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Docusaurus Tutorial',
+      items: [
+        'getting-started',
+        'create-a-page',
+        'create-a-document',
+        'create-a-blog-post',
+        'markdown-features',
+        'thank-you',
+        'hello-there'
+      ],
+    },
+  ],
+};
