57b6691101e7fdad5db0b402c2a6916cdbdd3208
swarn <swarnchahal@Swarns-MacBook-Pro.local>
2021-05-20 13:46:26 -0400

    add first blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ function Feature({imageUrl, title, description}) {
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
-      <h3>{title}</h3>
+      <h3 className={styles.title}>{title}</h3>
       <p>{description}</p>
     </div>
   );
