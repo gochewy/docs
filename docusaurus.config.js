f6bb7ebb4023d881279c94c027d1d54c6cbe34a3
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-10-18 22:32:22 -0400

    updated docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,11 @@ module.exports = {
   organizationName: 'gochewy', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
   themeConfig: {
-    hideableSidebar: true,
+    docs: {
+      sidebar: {
+        hideable: true,
+      }
+    },
     navbar: {
       title: 'Chewy Documentation',
       logo: {
